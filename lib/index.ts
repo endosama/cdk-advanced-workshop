7da7539fea52f1f586d97a2971f0581d12494ce8
Andrea Piras <piras.andrea.developer@gmail.com>
2021-03-05 00:09:21 +0100

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface MultiRegionS3CrrKmsCmkTargetProps {
+  // Define construct properties here
+}
+
+export class MultiRegionS3CrrKmsCmkTarget extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: MultiRegionS3CrrKmsCmkTargetProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
